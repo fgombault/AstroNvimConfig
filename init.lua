bc03e2540379cd1bd4f97eecb6c2bfaa183df1fc
François Gombault <francois.gombault@posteo.de>
2023-06-11 15:38:43 +0200

    fixed typo

--- init.lua
+++ init.lua
@@ -74,7 +74,7 @@ local config = {
     },
     {
       'beauwilliams/focus.nvim', -- window management and resizing
-      event = "BuffEnter",
+      event = "BufEnter",
       opts = { hybridnumber = true },
       config = function()
         require('focus').setup({ hybridnumber = true })
