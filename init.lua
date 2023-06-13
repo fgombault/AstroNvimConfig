396e6900097c61a423c367db50f1b94cc5b71936
François Gombault <francois.gombault@posteo.de>
2023-06-13 08:31:51 +0200

    old school statusline mode

--- init.lua
+++ init.lua
@@ -34,6 +34,29 @@ local config = {
       end
     },
     {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        local status = require("astronvim.utils.status")
+        opts.statusline = {
+          hl = { fg = "fg", bg = "bg" },
+          status.component.mode { mode_text =
+          { padding = { left = 1, right = 1 } } }, -- add the mode text
+          status.component.git_branch(),
+          status.component.file_info(),            -- shows filename & modified status
+          status.component.git_diff(),
+          status.component.diagnostics(),
+          status.component.fill(),
+          status.component.cmd_info(),
+          status.component.fill(),
+          status.component.lsp(),
+          status.component.treesitter(),
+          status.component.nav { scrollbar = false },
+          -- removed right-side mode color block
+        }
+        return opts
+      end,
+    },
+    {
       "Mofiqul/dracula.nvim", -- in case of issue, try catppuccin
       config = function()
         local dracula = require("dracula")
