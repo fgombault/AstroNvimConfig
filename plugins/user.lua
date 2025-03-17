a97a9f005fc0a512d600658797ba612f366bdd4a
François Gombault <francois.gombault@posteo.de>
2025-03-17 08:14:26 +0100

    neovim update = config rework

---
+++ plugins/user.lua
@@ -0,0 +1,231 @@
+local map = vim.keymap.set
+return {
+  {
+    'Exafunction/codeium.vim',
+    event = "BufReadPost",
+    config = function()
+      map('i', '<S-Right>', function()
+          return vim.fn['codeium#Accept']()
+        end,
+        { expr = true })
+      map('i', '<c-Right>', function()
+          return vim.fn['codeium#CycleCompletions'](1)
+        end,
+        { expr = true })
+      vim.g.codeium_tab_fallback = '' -- don't insert a tab
+      vim.g.codeium_idle_delay = 1000 -- avoid frantic suggestions
+      vim.cmd([[let g:codeium_filetypes = {
+  \ "nim": v:true,
+  \ "fish": v:false,
+  \ }]])
+    end
+  },
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require("astroui.status")
+      opts.statusline = {
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode { mode_text =
+        { padding = { left = 1, right = 1 } } }, -- add the mode text
+        status.component.git_branch(),
+        status.component.file_info(),            -- filename & modified status
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.nav { scrollbar = false },
+        -- removed right-side mode color block
+      }
+      return opts
+    end,
+  },
+  {
+    "Mofiqul/dracula.nvim", -- in case of issue, try catppuccin
+    config = function()
+      local dracula = require("dracula")
+      local c = dracula.colors()
+      c['bg'] = '#1A1A1A' -- the same as my terminal background
+      dracula.setup({
+        colors = c
+      })
+    end,
+  },
+  {
+    'tenxsoydev/tabs-vs-spaces.nvim',      -- detect indentation style
+    config = function()
+      require('tabs-vs-spaces').setup()
+    end,
+    event = "User Astrofile"     -- for plugins related to "real files"
+  },
+  {
+    'roxma/vim-paste-easy', -- paste without indent
+    event = "BufReadPost"
+  },
+  {
+    'mechatroner/rainbow_csv',
+    ft = { 'csv' }
+  },
+  {
+    'dag/vim-fish', -- highlighting for fish
+    ft = { 'fish' }
+  },
+  {
+    'alaviss/nim.nvim', -- highlighting for nim
+    ft = { 'nim' }
+  },
+  {
+  'akinsho/flutter-tools.nvim',
+    config = function()
+      require("flutter-tools").setup {} -- use defaults
+    end,
+    -- lazy = false,
+    event = "BufReadPost",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+  },
+  {
+    'NoahTheDuke/vim-just', -- highlighting for just
+    ft = { 'just' }
+  },
+  {
+    'AndrewRadev/inline_edit.vim', -- InlineEdit command
+    event = "BufReadPost"
+  },
+  {
+    'lukas-reineke/virt-column.nvim', -- discreet color column
+    event = "BufEnter",
+    opts = { char = '.' },
+  },
+  {
+    'beauwilliams/focus.nvim', -- window management and resizing
+    event = "BufEnter",
+    opts = { hybridnumber = true },
+    config = function()
+      require('focus').setup({ hybridnumber = true })
+      map("n", "g,", ":FocusSplitCycle<CR>", { desc = "Cycle Focus" })
+    end,
+  },
+  {
+    'ojroques/nvim-bufdel', -- no buffer deletion puzzles
+    event = "BufReadPost",
+    config = function()
+      require('bufdel').setup()
+      map("n", "<leader>Q", ":BufDel<CR>", { desc = "Delete Buffer" })
+      map("n", "<leader>q", ":BufDel<CR>:q<CR>",
+        { desc = "Delete Buffer and window" })
+    end,
+  },
+  {
+    'levouh/tint.nvim', -- unfocused windows are darker
+    event = "BufReadPost",
+    config = function()
+      require('tint').setup({
+        tint = -65,
+        highlight_ignore_patterns = { "WinSeparator", "Status.*" },
+      })
+      -- fix neotree/tint issue not giving colors back to the text buffer
+      map('n', '<leader>e', function()
+          vim.cmd([[Neotree toggle]])
+          local t = require("tint")
+          t.toggle()
+          t.toggle()
+        end,
+        { desc = "Toggle Explorer" })
+    end
+  },
+  {
+    'echasnovski/mini.jump2d', -- Jump around with ',' key
+    version = false,
+    event = "BufReadPost",
+    dependencies = { 'echasnovski/mini.nvim' },
+    config = function()
+      local jump = require("mini.jump2d")
+      local sp1 = jump.gen_pattern_spotter('[%w\'"]+', 'start')
+      local sp2 = jump.gen_pattern_spotter('[{,}]$', 'start')
+      local opts = {
+        spotter = jump.gen_union_spotter(sp1, sp2),
+        labels = "abcdefghiklmnopqrsuvwxy",
+        mappings = {
+          start_jumping = ',',
+        },
+        view = {
+          n_steps_ahead = 1,
+        },
+      }
+      jump.setup(opts)
+      vim.cmd([[hi MiniJump2dSpot guifg=#FFFFEE guibg=#DD2222]])
+      vim.cmd([[hi MiniJump2dSpotAhead guifg=#FFFFEE guibg=#B52222]])
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",                 -- highlight todos & jump
+    event = "BufReadPost",
+    dependencies = { "nvim-lua/plenary.nvim" }, -- + "brew install ripgrep"
+    opts = function(_, opts)
+      local td = require("todo-comments")
+      opts.highlight = {
+        throttle = 2000,
+        multiline = false,
+        comments_only = true,
+        pattern = { [[.*<(KEYWORDS)\s*:?]], },
+      }
+      return opts
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    event = "BufReadPost",
+    cmd = { "TroubleToggle", "Trouble" },
+    keys = {
+      { "<leader>x", desc = "Trouble" },
+      {
+        "<leader>i",
+        "<cmd>TodoTrouble<cr>",
+        desc = "Document TODO comments"
+      },
+      {
+        "<leader>x" .. "X",
+        "<cmd>TroubleToggle workspace_diagnostics<cr>",
+        desc = "Workspace Diagnostics (Trouble)"
+      },
+      {
+        "<leader>x" .. "x",
+        "<cmd>TroubleToggle document_diagnostics<cr>",
+        desc = "Document Diagnostics (Trouble)"
+      },
+      {
+        "<leader>x" .. "l",
+        "<cmd>TroubleToggle loclist<cr>",
+        desc = "Location List (Trouble)"
+      },
+      {
+        "<leader>x" .. "q",
+        "<cmd>TroubleToggle quickfix<cr>",
+        desc = "Quickfix List (Trouble)"
+      },
+    },
+    opts = {
+      use_diagnostic_signs = true,
+      action_keys = {
+        close = { "q", "<esc>" },
+        cancel = "<c-e>",
+      },
+    },
+  },
+  {
+    "romainl/vim-cool", -- prevent stale search highlighting
+    event = "BufReadPost",
+  },
+  {
+    "tikhomirov/vim-glsl", -- gl shader language
+    event = "BufReadPost",
+  },
+  -- other plugins to consider
+  -- emmet-vim, for expanding abbreviations (essential for web dev?)
+}
---
+++ polish.lua
@@ -0,0 +1,63 @@
+-- if true then return end -- COMMENT THIS LINE TO ACTIVATE THIS FILE
+
+-- This will run last in the setup process and is a good place to configure
+-- things like custom filetypes. This just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+-- Set up custom filetypes
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
+
+
+local map = vim.keymap.set
+-- cmd+S can save, this needs a terminal (kitty) config to send ctrl-s
+-- note: normal mode shortcut already exists in vanilla astronvim
+map("i", "<C-s>", "<Esc>:w!<CR>i", { desc = "Save in insert mode" })
+
+-- fix some colors
+vim.cmd([[colorscheme dracula]])
+vim.cmd([[hi WinSeparator ctermbg=NONE guibg=NONE guifg=#AA0000]])
+vim.cmd([[hi CursorLine guibg=#401A11]])
+require("notify").setup({ background_colour = "#000000" })
+
+vim.cmd([[set wrap]])
+
+map("n", "<leader>b", "<cmd>s/^ *//<cr>" ..
+  "<cmd>s/$/ /<cr>" ..
+  "o<esc><up>" ..
+  "<cmd>.!toilet -f pagga -w 77<cr>" ..
+  "<cmd>lua require('Comment.api').toggle.linewise(3)<cr>" ..
+  "<down><down><down>",
+  { desc = "Comment banner" })
+
+map('n', '<leader>gg', function()
+    require("astrocore").toggle_term_cmd "lazygit"
+  end,
+  { desc = "Toggle Lazygit" })
+
+vim.filetype.add(
+  {
+    extension = {
+      f = "glsl",
+      v = "glsl"
+    },
+  }
+)
+
+map('n', '<leader>tt',
+  "<cmd>TermExec size=10 direction=horizontal cmd='just tdd'<cr>",
+  { desc = "TDD" })
+
+require("luasnip.loaders.from_vscode").lazy_load(
+  { paths = { "./lua/vscode_snippets" } })
+require("luasnip").filetype_extend("just", { "sh" })
+
