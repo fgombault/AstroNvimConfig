6a6eb89415ca7b0d894d5c9ff23ffc225e1f7b6b
François Gombault <francois.gombault@posteo.de>
2025-03-31 09:04:53 +0200

    initial work for openrouter llm - not functional

--- plugins/llm.lua
+++ plugins/llm.lua
@@ -8,5 +8,40 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
     },
+  opts = function(_, opts)
+    opts.adapters = {
+      openrouter = function()
+        return require("codecompanion.adapters").extend("openai_compatible", {
+          env = {
+            url = "https://openrouter.ai/api",
+            -- api_key = os.getenv("LLM_KEY"),
+            chat_url = "/v1/chat/completions",
+          },
+          schema = {
+            model = {
+              default = "anthropic/claude-3.7-sonnet",
+            },
+          },
+        })
+      end
+    }
+    opts.strategies = {
+      chat = {
+        -- Change the default chat adapter
+        adapter = "openrouter",
+        },
+      inline = {
+        adapter = "openrouter",
+        },
+      cmd = {
+        adapter = "openrouter",
+        },
+      }
+    opts.opts = {
+      -- Set debug logging
+      log_level = "DEBUG",
+      }
+    return opts
+    end,
   },
 }
